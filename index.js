37705caf6ada17e0177fd1ad83e8be424df2ae56
unknown <riskeverything37@gmail.com>
2021-01-25 21:45:54 +0530

    add to heroku

---
+++ index.js
@@ -0,0 +1,76 @@
+//function to make  sound when icon pressed
+var numberOfDrumButton = document.querySelectorAll(".drum").length;
+//var audio=new Audio('sounds/tom-1.mp3');
+for (var i = 0; i < numberOfDrumButton; i++) {
+  document.querySelectorAll(".drum")[i].addEventListener("click", function() {
+
+    var buttonInnerHTML = this.innerHTML;
+    makesound(buttonInnerHTML);
+    buttonAnimation(buttonInnerHTML);
+  });
+} //for loop
+
+
+// detecting keyboard preess
+document.addEventListener("keypress", function(event) {
+  makesound(event.key);
+  buttonAnimation(event.key);
+  //document because we are listening all page
+});
+
+// fucntion to make sound for both keyboard and icon pressed
+
+function makesound(key) {
+  switch (key) {
+    case "w":
+      var tom1 = new Audio("sounds/tom-4.mp3");
+      tom1.play();
+      break;
+
+    case "a":
+      var tom2 = new Audio("sounds/tom-2.mp3");
+      tom2.play();
+      break;
+
+    case "s":
+      var tom3 = new Audio("sounds/tom-3.mp3");
+      tom3.play();
+      break;
+
+    case "d":
+      var tom4 = new Audio("sounds/tom-1.mp3");
+      tom4.play();
+      break;
+
+    case "j":
+      var snare = new Audio("sounds/snare.mp3");
+      snare.play();
+      break;
+
+    case "k":
+      var crash = new Audio("sounds/crash.mp3");
+      crash.play();
+      break;
+
+    case "l":
+      var kick = new Audio("sounds/kick-bass.mp3");
+      kick.play();
+      break;
+
+    default:
+  }
+
+}
+
+// animation to keys
+function buttonAnimation(currentKey){
+	var activeButton=document.querySelector("."+currentKey);
+	
+	activeButton.classList.add("pressed");
+
+	setTimeout(function(){
+		activeButton.classList.remove("pressed");
+	},100);
+	//setTimeOut inbuilt function for timeout,with parameter of function and timeout(100 here)
+	//timeout parameter (how much time to WAIT before execute this function)
+}
